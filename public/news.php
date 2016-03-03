da2651a4ade7e5c292f0fa57f2b05694817f58cf
Terry Gilliver <gillivt@hotmail.co.uk>
2016-03-03 20:21:00 +0000

    added news functionality

--- public/news.php
+++ public/news.php
@@ -1,5 +1,28 @@
 <?php
 require_once("../database/initialize.php");
+
+// 1. the current page number ($current_page)
+$page = !empty($_GET['page']) ? (int) $_GET['page'] : 1;
+
+// 2. records per page ($per_page)
+$per_page = 6;
+
+// 3. total record count ($total_count)
+$total_count = News::count_all();
+
+$pagination = new Pagination($page, $per_page, $total_count);
+
+// Instead of finding all records, just find the records 
+// for this page
+$sql = "SELECT * FROM news ";
+$sql .= "LIMIT {$per_page} ";
+$sql .= "OFFSET {$pagination->offset()}";
+$news = News::find_by_sql($sql);
+
+// Need to add ?page=$page to all links we want to 
+// maintain the current page (or store $page in $session)
+
+
 include_layout_template("header.php");
 ?>
 <!--
@@ -23,25 +46,55 @@ Modification History:
                 <br><br>
                 <a href="https://www.facebook.com/The-Plough-Ashby-De-La-Zouch-1409446652610698/"><img class="img-responsive" src="assets/images/facebook.png" alt="facebook"></a>
             </div>
-
-            <div class="col-sm-12 col-md-11"><h1>News</h1>                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <p>
-                    Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
-                </p>
-                <p>
-                    Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Vivamus facilisis diam at odio. Mauris dictum, nisi eget consequat elementum, lacus ligula molestie metus, non feugiat orci magna ac sem. Donec turpis. Donec vitae metus. Morbi tristique neque eu mauris. Quisque gravida ipsum non sapien. Proin turpis lacus, scelerisque vitae, elementum at, lobortis ac, quam. Aliquam dictum eleifend risus. In hac habitasse platea dictumst. Etiam sit amet diam. Suspendisse odio. Suspendisse nunc. In semper bibendum libero.
-                </p>
-                <p>
-                    Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
-                </p>
-            </div>            
-
+            <div class="col-sm-12 col-md-11">
+                <?php foreach ($news as $newsItem): ?>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4">
+                            <h1><?php echo $newsItem->newsTitle ?></h1>
+                            <img class="img-responsive" src="<?php echo $newsItem->imageURL ?>">
+                        </div>
+                        <div class="col-sm-12 col-md-8">
+                            <h1>&nbsp;</h1>
+                            <p><?php echo $newsItem->newsContent ?></p>
+                            <a href="<?php echo $newsItem->linkAddress ?>"><?php echo $newsItem->linkTitle ?></a>
+                        </div>
+                    </div>
+                <hr>
+                <?php endforeach; ?>
+            </div>
         </div>
+        <div id="pagination" style="clear: both;">
+            <?php
+            if ($pagination->total_pages() > 1) {
+
+                if ($pagination->has_previous_page()) {
+                    echo "<a class=\"btn btn-primary\" role=\"button\" href=\"news.php?page=";
+                    echo $pagination->previous_page();
+                    echo "\">&laquo; Previous</a> ";
+                }
+
+                for ($i = 1; $i <= $pagination->total_pages(); $i++) {
+                    if ($i == $page) {
+                        echo " <span class=\"selected\">{$i}</span> ";
+                    } else {
+                        echo " <a class=\"btn btn-primary\" role=\"button\" href=\"news.php?page={$i}\">{$i}</a> ";
+                    }
+                }
+
+                if ($pagination->has_next_page()) {
+                    echo " <a class=\"btn btn-primary\" role=\"button\" href=\"news.php?page=";
+                    echo $pagination->next_page();
+                    echo "\">Next &raquo;</a> ";
+                }
+            }
+            ?>
+        </div>
+
+
+
     </div>
 </main>
-        
+
 <script src="assets/js/news.js"></script>
 <?php
 include_layout_template("footer.php");
